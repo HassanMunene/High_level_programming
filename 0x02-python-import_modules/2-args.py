31e27bf9ffb7490112a9765bc4d956a2bda7407e
HassanMunene <awanzihassan@gmail.com>
2023-01-11 11:59:43 +0000

    correction of ambigous variable

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 
 if __name__ == "__main__":
     import sys
-    l = sys.argv
-    size = len(l) - 1
+    le = sys.argv
+    size = len(le) - 1
     ar = "argument" if size == 1 else "arguments"
     dot = "." if size == 0 else ":"
     print("{} {}".format(size, ar + dot))
     for i in range(1, size + 1):
-        print("{}: {}".format(i, l[i]))
+        print("{}: {}".format(i, le[i]))
