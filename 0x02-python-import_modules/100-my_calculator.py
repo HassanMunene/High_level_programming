c6cdbb4d76c33086d8710ed5712ca0b972dc3c8a
HassanMunene <awanzihassan@gmail.com>
2023-01-11 12:12:54 +0000

    a couple of typing error orrections

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,17 +4,17 @@ if __name__ == "__main__":
     import sys
     import calculator_1 as calc
     ops = ["+", "-", "*", "/"]
-    l = sys.argv
-    if len(l) != 4:
+    le = sys.argv
+    if len(le) != 4:
         print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
         exit(1)
-    if not l[2] in ops:
-        print("{}".format("Uknown operator. Available operators: +, -, *, /"))
+    if not le[2] in ops:
+        print("{}".format("Unknown operator.Available operators: +, -, * and /"))
         exit(1)
-    a = int(l[1])
-    b = int(l[3])
-    op = l[2]
-    result = (calc.add(a, b) if ops == "+" else
+    a = int(le[1])
+    b = int(le[3])
+    op = le[2]
+    result = (calc.add(a, b) if op == "+" else
     calc.sub(a, b) if op == "-" else
     calc.mul(a, b) if op == "*" else
     calc.div(a, b) if op == "/" else 0)
