94187336ad39527190627258f2a685b4b54dab34
HassanMunene <awanzihassan@gmail.com>
2023-01-11 14:52:53 +0000

    more corections

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-#declare a variable that we will use to store the concatenated alphabets
-result = ""
-#use for loop to iterate over the ASCII values
-#use chr() function to produce the character representation of the ASCII value
-for i in range(97, 123):
-    result = result + chr(i)
-    
-print("{}".format(str(result)), end="")
+
+for i in range(97, 123, 1):
+    print("{}".format(chr(i)), end="")
 
