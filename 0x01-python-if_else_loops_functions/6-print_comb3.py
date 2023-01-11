12d589ab171eeeb5c44223ff8b5711b976d0b63e
HassanMunene <awanzihassan@gmail.com>
2023-01-11 15:24:32 +0000

    pycodestyle correction

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 for i in range(0, 100, 1):
-    f = int (i / 10)
-    g = int (i % 10)
+    f = int(i / 10)
+    g = int(i % 10)
     if f == g:
         continue
     if f > g:
