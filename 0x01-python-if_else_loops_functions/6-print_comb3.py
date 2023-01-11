7365a0335c3088a6901aec8192516ce74dc799da
HassanMunene <awanzihassan@gmail.com>
2023-01-11 15:22:01 +0000

    the brain is very important

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+for i in range(0, 100, 1):
+    f = int (i / 10)
+    g = int (i % 10)
+    if f == g:
+        continue
+    if f > g:
+        continue
+    if i == 89:
+        print("{}{}".format(f, g))
+        break
+    print("{}{}, ".format(f, g), end="") 
