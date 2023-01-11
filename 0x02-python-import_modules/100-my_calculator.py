569bb878f0bdd25ee8669b359290b1511a7d38ab
HassanMunene <awanzihassan@gmail.com>
2023-01-11 12:20:59 +0000

    commiting

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,14 +9,14 @@ if __name__ == "__main__":
         print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
         exit(1)
     if not le[2] in ops:
-        print("{}".format("Unknown operator.Available operators: +, -, * and /"))
+        print("{}".format("Unknown operator. Available operators: +, -, * and /"))
         exit(1)
     a = int(le[1])
     b = int(le[3])
     op = le[2]
     result = (calc.add(a, b) if op == "+" else
-    calc.sub(a, b) if op == "-" else
-    calc.mul(a, b) if op == "*" else
-    calc.div(a, b) if op == "/" else 0)
+                calc.sub(a, b) if op == "-" else
+                calc.mul(a, b) if op == "*" else
+                calc.div(a, b) if op == "/" else 0)
     
     print("{} {} {} = {}".format(a, op, b, result))
