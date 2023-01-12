e8af35fdd56c2a78868cd04f7cc2363a0ed51328
HassanMunene <awanzihassan@gmail.com>
2023-01-12 13:02:17 +0000

    typing error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,6 +7,6 @@ def print_matrix_integer(matrix=[[]]):
         l2 = len(matrix[r])
         for c in range(l2):
             if c == l2 - 1:
-                print("{:d} ".format(matrix[r][c]))
+                print("{:d}".format(matrix[r][c]))
             else:
                 print("{:d} ".format(matrix[r][c]), end="") 
