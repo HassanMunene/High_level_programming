15deac01f895f9f135620b4dd94a2fa93d61e308
HassanMunene <hassanmunene@ubuntu.hassan.com>
2023-02-08 23:56:20 +0300

    correction of naming error

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module checks whether an object is an instance of
+a class that inherited directly or indirectly from a
+specified class
+"""
+
+def inherits_from(obj, a_class):
+    """
+    a func to achieve the target of the module
+    """
+    return isinstance(obj, a_class) and not type(obj) == a_class
