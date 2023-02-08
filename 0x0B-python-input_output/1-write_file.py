af087f05e1540da75a8042c011a90b36fcd67d7d
HassanMunene <hassanmunene@ubuntu.hassan.com>
2023-02-09 00:13:54 +0300

    pycodestyele correction

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """
 This module contains a fucntion that writes a string to
 a txt file
-if the file does not exist it creates the file and if the 
+if the file does not exist it creates the file and if the
 file contains texts it overites them
 """
 
@@ -13,4 +13,3 @@ def write_file(filename="", text=""):
     """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
-
