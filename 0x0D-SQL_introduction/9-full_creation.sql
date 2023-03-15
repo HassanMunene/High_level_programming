3ac293e04c53bcc41a1e25d0236d0ef0327cee3b
HassanMunene <awanzihassan@gmail.com>
2023-03-15 10:39:38 +0300

    corections

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -8,10 +8,10 @@
 --      id = 3,"Bob" , score = 14
 --      id = 4, "Gorge", score = 8
 
-CREATE TABLE IF NOT EXISTS second_table(id INT, 
+CREATE TABLE IF NOT EXISTS second_table (id INT, 
 name VARCHAR(256), score INT);
 INSERT INTO second_table(id, name, score)
-VALUES(1, "John", 10),
+VALUES (1, "John", 10),
 (2, "Alex", 3),
 (3, "Bob", 14),
 (4, "Gorge", 8);
