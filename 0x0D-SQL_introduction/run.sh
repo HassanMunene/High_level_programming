6e457bb2364c1b0991dae34ca445d7c06001ce9b
HassanMunene <awanzihassan@gmail.com>
2023-03-15 06:16:06 +0300

    the run

--- 0x0D-SQL_introduction/run.sh
+++ 0x0D-SQL_introduction/run.sh
@@ -1,3 +1,8 @@
- git add .
- git commit -m "$1"
- git push
+# This is a script that I have created to automate the process of git add, commit and push
+# The commit message is written inside double quotes at the command line
+# To run the script type at the command line the following
+# ./run.sh "Your commit message" press enter
+
+git add .
+git commit -m "$1"
+git push
