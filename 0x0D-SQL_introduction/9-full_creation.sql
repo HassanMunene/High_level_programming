9800be9def40115025556a146dab918a1acf4ae7
HassanMunene <awanzihassan@gmail.com>
2023-03-15 10:53:33 +0300

    more corrections on inserting into database table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -8,11 +8,9 @@
 --      id = 3,"Bob" , score = 14
 --      id = 4, "Gorge", score = 8
 
-CREATE TABLE IF NOT EXISTS second_table (id INT, 
-name VARCHAR(256), score INT);
-INSERT INTO second_table(id, name, score)
-VALUES (1, "John", 10),
-(2, "Alex", 3),
-(3, "Bob", 14),
-(4, "Gorge", 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "Gorge", 8);
 
