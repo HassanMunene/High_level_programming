e1095577f4d18b472a877fa78d62ac6055acc5d2
HassanMunene <awanzihassan@gmail.com>
2023-03-16 18:39:23 +0300

    how to list the privilages different users have been granted

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- a script that lists all the privilages of 
+-- mysql users that is user_0d_1 and user_0d_2 on your server
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
