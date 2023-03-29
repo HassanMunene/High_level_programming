8f7ad786ed5e8b5f335d432dcc8c50a23d306a5c
HassanMunene <awanzihassan@gmail.com>
2023-03-29 17:59:59 +0300

    one of its kind

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,30 @@
+"""
+This module adds two integers that is a and b
+a and b must be integers or floats, otherwise raise TypeError exception
+with message "a must be an integer or b must be an integer
+if a and b are floats they must be first casted to integers
+"""
+def add_integer(a, b=98):
+    """
+    will be used two add two integers and
+    return results
+    """
+    if type(a) is float:
+        a = int(a)
+    else:
+        a = a
+    
+    if type(b) is float:
+        b = int(b)
+    else:
+        b = b
+
+    if a is None or type(a) is not int:
+        raise TypeError("a must be an integer")
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+
+    return a + b
+
+
+
