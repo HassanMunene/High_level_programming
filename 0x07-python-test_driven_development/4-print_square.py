c8c39f4d589adc807253c4f70199380f7c2431dd
HassanMunene <awanzihassan@gmail.com>
2023-03-30 12:28:55 +0300

    great work drawing a square without really googling never thought i'd be able to do this

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+This module print a square made up of #
+the size in this case is the length of the square
+and as you know the length of the square is equal on
+both sides
+"""
+
+
+def print_square(size):
+    """
+    this is the damn function
+    """
+
+    if type(size) is not int:
+        raise TypeError("size must be integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
