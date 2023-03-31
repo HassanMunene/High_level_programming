859327d761bae0e333f244eb8d665b95f7fd79e9
HassanMunene <awanzihassan@gmail.com>
2023-03-31 15:00:42 +0300

    writing the serialized object to a json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+In this module we are saving the object
+into a text file using json representation
+"""
+import json
+
+
+def save_to_json_file(my_dict, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        json_string = json.dumps(my_dict)
+        f.write(json_string)
