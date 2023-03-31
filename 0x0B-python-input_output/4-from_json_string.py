9231ddf43ae84e9a77f79a193a477419db4814a3
HassanMunene <awanzihassan@gmail.com>
2023-03-31 14:11:17 +0300

    correcting pycodestyle

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+import json
 """
 This file contains a function that accepts a json string
 and converts the string to a python object
 """
-import json
+
 
 def from_json_string(my_str):
     """
