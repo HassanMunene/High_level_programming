c05e92c8c971d198ebda6c1b9f25aec27040b0b5
HassanMunene <awanzihassan@gmail.com>
2023-03-31 14:08:06 +0300

    how to convert json string to python data

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This file contains a function that accepts a json string
+and converts the string to a python object
+"""
+import json
+
+def from_json_string(my_str):
+    """
+    takes in my_str which is json and
+    converts it to python object
+    """
+    python_obj = json.loads(my_str)
+    return python_obj
