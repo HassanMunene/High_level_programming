78c4c0ff50dcda8fec9cc2f663140016f5e26a4b
HassanMunene <awanzihassan@gmail.com>
2023-03-31 15:06:58 +0300

    adding documnetation

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,6 +7,9 @@ import json
 
 
 def save_to_json_file(my_dict, filename):
+    """
+    This is the function that does the job of serializing
+    """
     with open(filename, 'w', encoding='utf-8') as f:
         json_string = json.dumps(my_dict)
         f.write(json_string)
