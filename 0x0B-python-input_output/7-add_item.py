823c3987a2ea6d7db43a4962e31b0420c40a9a15
HassanMunene <awanzihassan@gmail.com>
2023-04-01 15:55:05 +0300

    correcting line too long pycodestyle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -15,9 +15,9 @@ import sys
 
 n = len(sys.argv)
 
-# using the for loop we iterate through the arguments and then append them to a list.
+# using for loop we iterate through the arguments and then append them to a list.
 # so we need to create an empty list first, after assigning them arguments
-# then next it to use that list to save it to a json file name add_item.json in a json representation
+# then use that list to save it to a json file name add_item.json in json
 
 cli_list = []
 
