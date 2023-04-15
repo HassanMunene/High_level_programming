f557b024b6c8c6915f52adc8e95fcfccd5dbcd03
HassanMunene <awanzihassan@gmail.com>
2023-04-15 14:02:49 +0300

    how to add more methods to the class

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
 """
-A MODULE containing a class rectangle
+A module containig a class rectangle that inherits from
+BaseGeometry
+which is defined in a file in the same directory
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """
-    a blueprint of rectangle
+    blueprint of a rectangle
     """
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+    
     def area(self):
         """
-        a func to find the area of the rectangle
+        return the area of rectangle
         """
-        return (self.__width) * (self.__height)
-    def print():
-        """
-        print the rectangle area
-        """
-        print("[Rectangle] {}/{}".format(self.__width, self.__height))
+        return self.__width * self.__height
 
     def __str__(self):
         """
-        magic method
+        print string representation of the object
         """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-        
+        return "[{}] {}/{}".format(self.__class__.__name__, self.__width,self.__height)
