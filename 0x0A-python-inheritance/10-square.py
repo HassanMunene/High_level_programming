5e7e4e9c6b2d783b8b0018925426383605cc5eb8
HassanMunene <awanzihassan@gmail.com>
2023-04-15 14:27:19 +0300

    how to calculate the area of the square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,14 +9,18 @@ class Square(Rectangle):
     """
     The blueprint of square
     """
-    def __init__(self,size):
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+    def __init__(self, size):
         self.__size = size
+        self.integer_validator("size", size)
+
+    def __str__(self):
+        """
+        return the string representation of the object
+        """
+        return f"[{self.__class__.__name__}] {self.__size}/{self.__size}"
 
     def area(self):
         """
-        a func to return the area of the square
+        calculate the area of the circle
         """
-        return self.__size ** 2
-
+        return self.__size * self.__size
