105572f19a2120a8693f13da676277919a6e120f
HassanMunene <awanzihassan@gmail.com>
2023-04-15 13:48:56 +0300

    that's more like it

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,6 +2,7 @@
 """
 A module containig a class rectangle that inherits from
 BaseGeometry
+which is defined in a file in the same directory
 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
@@ -12,9 +13,7 @@ class Rectangle(BaseGeometry):
     blueprint of a rectangle
     """
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
