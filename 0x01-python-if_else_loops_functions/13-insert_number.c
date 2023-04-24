3469dcd7cf31d5570a33cd55f2fd31c2343b49aa
HassanMunene <awanzihassan@gmail.com>
2023-04-24 18:24:31 +0300

    how to insert a node in a sorted singly linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -3,38 +3,56 @@
 #include "lists.h"
 
 /**
- * print_listint - print all the elements of a listint_t list
- * @h: pointer to the head of the list
- * return: the number of nodes
+ * insert_node - the function that does the insterting of the node
+ * @head: the pointer to the pointer of the head
+ * @number: the number to be insterted
+ * Return: the address of the new node or NULL if it failed
  */
 
-size_t print_listint(const listint_t *h)
-{
-	const listint_t *current;
-	unsigned int n;
-
-	current = h;
-	n = 0;
-	while (current != NULL)
-	{
-		printf("%i\n", current->n);
-		current = current->next;
-		n++;
-	}
-	return (n);
-}
-
-/**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
- * Return: address of the new element or Null if it fails
- */
-
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new;
 	listint_t *current;
+	listint_t *previous = NULL;
 
 	current = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = number;
+	new->next = NULL;
+
+	if (current == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (current != NULL)
+		{
+			if (number < current->n)
+			{
+				if (previous == NULL)
+				{
+					new->next = current;
+					*head = new;
+				}
+				else
+				{
+					previous->next = new;
+					new->next = current;
+				}
+				break;
+			}
+			previous = current;
+			current = current->next;
+		}
+		if (current == NULL)
+		{
+			previous->next = new;
+		}
+	}
+	return (new);
 }
