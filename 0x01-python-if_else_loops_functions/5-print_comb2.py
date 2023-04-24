e6aea734c50fa6b0d891a93afe29be10e028463b
HassanMunene <awanzihassan@gmail.com>
2023-04-24 12:48:02 +0300

    more of the correction

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """
-will be used to print mubers from 00 to 99
-numbers should be printed in asecending order with 2 digits
-each number should be separated by , and space
-the last number shoulld be followed by a new line
+a program that prints number from 0 to 99
 """
-for i in range(0, 100):
-    if i < 10:
-        print("0{}, ".format(i), end="")
-    elif i > 10 and i < 99:
-        print ("{}, ".format(i), end="")
-    elif i == 99:
-        print(f"{99}")
+
+numbers = ""
+for i in range(100):
+    numbers += "{:02d}".format(i)
+    if i < 99:
+        numbers += ", "
+
+print(numbers)
 
