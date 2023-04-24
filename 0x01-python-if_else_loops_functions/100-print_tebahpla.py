1d60b6e611f29051dd10b65d9ada406943529847
HassanMunene <awanzihassan@gmail.com>
2023-04-24 18:33:54 +0300

    program to print alphabet in reverse order while alternating uppecase and lowercase

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+prints ASCII alphabet in reverse order
+alternating lowercase and uppercase(z lower and Y upper)
+you are not allowed to store characters in a variable
+not allowed to import any module
+output: zYxWvUtSrQpOnMlKjIhGfEdCbA
+"""
+
