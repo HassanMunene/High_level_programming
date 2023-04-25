5eee31a1827ed1b4b21482ba70d5ee6410ca9b90
HassanMunene <awanzihassan@gmail.com>
2023-04-25 18:18:51 +0300

    making sure the module is not run when imported

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -20,4 +20,7 @@ b = 5
 print("{} + {} = {}".format(a, b, add(a, b)))
 print("{} - {} = {}".format(a, b, sub(a, b)))
 print("{} * {} = {}".format(a, b, mul(a, b)))
-#print("{} / {} = {}".format(a, b, div(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
+
+if __name__ == '__main__':
+    main()
