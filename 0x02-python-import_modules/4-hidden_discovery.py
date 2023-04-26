f85db3d6e3733f6e6d6e8d5a1116d852e9f4b465
HassanMunene <awanzihassan@gmail.com>
2023-04-26 11:13:23 +0300

    reading the compiled file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-
+"""
+this program prints all the names defined by the compiled
+module that is going to be imported here
+it should print one name per line
+it should print only names that do not start with __
+"""
 if __name__ == "__main__":
     import hidden_4
-    for i in dir(hidden_4):
-        if i[:2] == "__":
-            continue
-        print("{}".format(i))
+    import types
+    import importlib.util
+
+    module = types.ModuleType("hidden_4")
+    code = hidden_4.__code__
+    exec(code, module.__dict__)
+
+    for name in sorted(dir(module)):
+        if not name.startswith("__"):
+            print(name)
