c6ed5204303b756824bde1eac282249931228f11
HassanMunene <awanzihassan@gmail.com>
2023-04-27 11:56:15 +0300

    this script find the average temp by city and then order 3 of the top

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,9 @@
+-- displays top 3 cities temperatures during july and august
+-- ordered by temperatures(descending)
+
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month IN (7,8)
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
