92aa2aeeee472fc84bc6bff4a60ce6dde31d019e
HassanMunene <awanzihassan@gmail.com>
2023-04-27 15:48:08 +0300

    I have changed the keyword from PRIVILAGES to PRIVILEGES

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,5 +3,5 @@
 -- the password of the user should be set to user_0d_1_pwd
 -- if the user already exists, your script should still not fail
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
