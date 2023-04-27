7481d0cbd9b71bc086a8ee523da4e7edca486b6e
HassanMunene <awanzihassan@gmail.com>
2023-04-27 17:49:41 +0300

    a script to create a database and a table

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,10 @@
--- the script creates a database hbtn_0d_2 and user_0d_2
--- the user should only have SELECT privilage in the database hbtn_0d_2
+-- the script creates a database hbtn_0d_2
+-- the script also creates user_0d_2
+-- the user should only have the SELECT privilage in the database hbtn_0d_2
 -- the user password should be set to user_0d_2_pwd
--- if the database already exists your script should'nt fail and the same for user
+-- if the database already exists your script should'nt fail
+-- if the user alredy exists the script should not fail
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER OF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
--- create database hbtn_0d_usa and the table states
+-- create database hbtn_0d_usa
+-- create also a table called states
 -- id INT UNIQUE AUTOGEN NOT NULL PK, name VARCHAR(256)
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT NOT NULL PRIMARY KEY AUTO_IINCREMENT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
