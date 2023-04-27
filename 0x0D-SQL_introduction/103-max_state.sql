2a8be2b35a0f84b41a54a1347a3ab459efb56062
HassanMunene <awanzihassan@gmail.com>
2023-04-27 12:20:22 +0300

    finding the highest temperature in each state

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,7 +1,7 @@
 -- this script displays the max tempererature of each state
 -- ordered by state name
 
-SELECT State, MAX(value) AS max_temp
+SELECT state, MAX(value) AS max_temp
 FROM temperatures
-GROUP BY State
-ORDER BY State
+GROUP BY state
+ORDER BY state
