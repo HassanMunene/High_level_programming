9001630598a5d3963200fe000fee17d44a0c607e
HassanMunene <awanzihassan@gmail.com>
2023-04-28 13:29:45 +0300

    we have joined two tables where the id of one table is equal to the id of another table and the id is not null

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,6 +4,8 @@
 -- sort in asending order by title and genre_id
 -- use only on select statement
 
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres
-WHERE tv_show_genres.show_id = tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id 
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NOT NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
