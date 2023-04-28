1e7e32765a18492458143e94eb4db7fff2ea358b
HassanMunene <awanzihassan@gmail.com>
2023-04-28 11:49:38 +0300

    using the select statement to display the cities of a state using the foreign key

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,5 @@
 -- script creates table unique_id
+-- the id should be unique
 -- id INT DEFAULT 1 UNIQUE, name VARCHAR(256)
 
 CREATE TABLE IF NOT EXISTS unique_id(id INT UNIQUE DEFAULT 1, name VARCHAR(256));
