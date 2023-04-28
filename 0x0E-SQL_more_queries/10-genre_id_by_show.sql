7a9f652461d86fc1df6a0f30f2990d139e59b670
HassanMunene <awanzihassan@gmail.com>
2023-04-28 13:18:49 +0300

    a really complicated solution that i do not even know how to expain

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,9 @@
 -- import databse dump from hbtn_0d_tvshows
 -- write a script that lists all shows contained in hbtn_0d_tvshows
--- each row should display
+-- each record should display tv_show.title and tv_show_genre.genre_id
+-- sort in asending order by title and genre_id
+-- use only on select statement
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres
+WHERE tv_show_genres.show_id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -4,5 +4,5 @@
 
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON cities.state_id=states.id
+JOIN states ON cities.state_id=states.id
 ORDER BY cities.id;
