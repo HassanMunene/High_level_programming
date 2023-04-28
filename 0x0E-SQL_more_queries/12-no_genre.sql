6fc377b3356b3c50eaf9943cf561aa3a015c7752
HassanMunene <awanzihassan@gmail.com>
2023-04-28 21:37:11 +0300

    using the left join sql command with the a condition

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,7 +3,9 @@
 -- the result must be sorted in ascending order by title and genre_id
 -- you can only use one select statement
 
-SELECT tv_shows.title, tv_shows_genres.genre_id
-from tv_shows
-RIGHT JOIN tv_shows_genres
-ON tv_shows.id = tv_show_genre.show_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id is NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
