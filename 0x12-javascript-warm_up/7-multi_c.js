79f7cef46750c1681a79c3cb0c0d1a42011e5871
HassanMunene <awanzihassan@gmail.com>
2023-06-15 08:42:27 +0300

    nested for loops to print a square of characters

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,12 +3,10 @@ const { argv } = require('process');
 
 if (isNaN(Number(argv[2]))) {
   console.log('Missing number of occurrences');
-} else 
-{
-	let i = 1;
-	let x = Number(argv[2]);
-	for (i; i <= x; i++)
-	{
-		console.log('C is fun');
-	}
+} else {
+  let i = 1;
+  const x = Number(argv[2]);
+  for (i; i <= x; i++) {
+    console.log('C is fun');
+  }
 }
