78979d5c31c286518dbc0be25a6a8f5fd53c173a
HassanMunene <awanzihassan@gmail.com>
2023-06-16 18:21:43 +0300

    filter states by the first letter 'N'

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module connects to the database at localhost throught
+port 3306 and filters the table states by listing the states that
+starts with the letter "N"
+and orders them by id
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", user=username, password=password, database=database, port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    query_results = cur.fetchall()
+    for row in query_results:
+        print(row)
+
+    cur.close()
