37b1db61f38c13daccf61bbbb7a3e978c76dfa0e
HassanMunene <awanzihassan@gmail.com>
2023-06-17 09:30:10 +0300

    syntax correction

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,18 +7,19 @@ name with the name provided in the argv[4]
 import sys
 import MySQLdb
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-state_name = sys.argv[4]
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-db = MySQLdb.connect(host="localhost", user = username, password=password, database=database, port=3306)
+    db = MySQLdb.connect(host="localhost", user = username, password=password, database=database, port=3306)
 
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name = 'state_name' ORDER BY id ASC")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = 'state_name' ORDER BY id ASC")
 
-result_query = cur.fetchall()
-for row in result_query:
-    print(row)
+    result_query = cur.fetchall()
+    for row in result_query:
+        print(row)
 
-db.close()
+    db.close()
