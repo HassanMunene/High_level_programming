7ee5789e497e921a7f4fb612c4ef512f954d2736
HassanMunene <awanzihassan@gmail.com>
2023-06-17 09:28:40 +0300

    filter the argument provided in the cli

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This script filters the states in the
+hbtn_0e_0_usa database by matching their
+name with the name provided in the argv[4]
+"""
+import sys
+import MySQLdb
+
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+state_name = sys.argv[4]
+
+db = MySQLdb.connect(host="localhost", user = username, password=password, database=database, port=3306)
+
+cur = db.cursor()
+cur.execute("SELECT * FROM states WHERE name = 'state_name' ORDER BY id ASC")
+
+result_query = cur.fetchall()
+for row in result_query:
+    print(row)
+
+db.close()
