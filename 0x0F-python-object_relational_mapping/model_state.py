e4fab810a931c042342df962e8eed519bc16b707
HassanMunene <awanzihassan@gmail.com>
2023-06-18 18:26:14 +0300

    pycode style

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,6 @@
 """
-using sqlalchemy
+using sqlalchemy to create a table
+using class and then connecting to the mysql
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
