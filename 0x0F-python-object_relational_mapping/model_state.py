99774c2437bd4ec38f98aa84ba1b64dedbf24e84
HassanMunene <awanzihassan@gmail.com>
2023-06-18 18:53:27 +0300

    correcting a requirement

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,16 +6,16 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
-Base = declarative_base()
-
-class State(Base):
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
-    name = Column(String(128), nullable=False)
-
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}')
-
 if __name__ == "__main__":
-    from model_state import State
 
+    Base = declarative_base()
+
+    class State(Base):
+        __tablename__ = 'states'
+        id = Column(Integer, primary_key=True, nullable=False, unique=True)
+        name = Column(String(128), nullable=False)
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}')
+
+    from model_state import State
     Base.metadata.create_all(engine)
