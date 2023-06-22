c9f3568ab34e213164f05bf7740b797b4715239c
HassanMunene <awanzihassan@gmail.com>
2023-06-22 20:59:52 +0300

    deleting a row from the table

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This modlue id used to delete rows from the table states\
+which contain letter 'a' in their name
+"""
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+user = sys.argv[1]
+pwd = sys.argv[2]
+db = sys.argv[3]
+
+engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@localhost:3306/{db}')
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+results = session.query(State).filter(State.name.like ("%a%")).all()
+
+for result in results:
+    session.delete(result)
+
+session.commit()
