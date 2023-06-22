987954d4f4bfeee03e188eaeaf8e7bb5a63fe2bd
HassanMunene <awanzihassan@gmail.com>
2023-06-22 08:56:18 +0300

    adding shebang line

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,12 +3,18 @@
 using sqlalchemy to create a table
 using class and then connecting to the mysql
 """
+import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class State(Base):
+    """
+    this is the class that represents
+    the table states, the attributes rep
+    the columns of the table
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
