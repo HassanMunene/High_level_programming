75919be4f72d42a97ed4a18ac983d79986cf0e1e
HassanMunene <awanzihassan@gmail.com>
2023-06-22 10:43:00 +0300

    more queries

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module recives an argument in the cli and then
+filters the table in the hbtn_0e_6_usa db using the arg
+as a name and the prints the id of the object retrieve if foun
+or "Not found" if it does not match
+"""
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+user = sys.argv[1]
+pwd = sys.argv[2]
+db = sys.argv[3]
+sysname = sys.argv[4]
+
+engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@localhost:3306/{db}')
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+result = session.query(State).filter(State.name == sysname).first()
+
+if result:
+    print(result.id)
+else:
+    print("Not found")
---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module filters the table and returns
+data whose name has a letter "a" in it
+it then orders the data by id in ascending order
+"""
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+
+engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@localhost:3306/{db}')
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+results = session.query(State)\
+    .filter(State.name.like('%a%')).order_by(State.id)
+
+for query in results:
+    print('{}: {}'.format(query.id, query.name))
