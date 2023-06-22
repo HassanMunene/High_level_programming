b9e834c336040c09035a0daf39c4f0df54f79f91
HassanMunene <awanzihassan@gmail.com>
2023-06-22 21:01:09 +0300

    deleting a row from the table

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -9,18 +9,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-user = sys.argv[1]
-pwd = sys.argv[2]
-db = sys.argv[3]
+if __name__ == "__main__":
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
 
-engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@localhost:3306/{db}')
+    engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@localhost:3306/{db}')
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-results = session.query(State).filter(State.name.like ("%a%")).all()
+    results = session.query(State).filter(State.name.like ("%a%")).all()
 
-for result in results:
-    session.delete(result)
+    for result in results:
+        session.delete(result)
 
-session.commit()
+    session.commit()
