3f9db04b5c0188b4cee1702571c6c3d41abe81bd
HassanMunene <awanzihassan@gmail.com>
2023-06-22 19:25:58 +0300

    updating the table

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module will be used to update the table
+states where the id is 2 from its current name
+to New Mexico
+"""
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+user = sys.argv[1]
+pwd = sys.argv[2]
+db = sys.argv[3]
+
+engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@localhost:3306/{db}')
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+state_obj = session.query(State).filter_by(id=2).first()
+state_obj.name = 'New Mexico'
+
+session.commit()
