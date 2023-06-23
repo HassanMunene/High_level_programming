c7d24591bfd8a88ed74130d870707b5635879e9b
HassanMunene <awanzihassan@gmail.com>
2023-06-23 12:02:35 +0300

    syntax correction

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -11,19 +11,20 @@ from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy.orm import sessionmaker
 
-user = sys.argv[1]
-pwd = sys.argv[2]
-db = sys.argv[3]
+if __name__ == "__main__":
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
 
-engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@localhost:3306/{db}')
-Base.metadata.create_all(engine)
+    engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@localhost:3306/{db}')
+    Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-cities = session.query(City).order_by(City.id).all()
+    cities = session.query(City).order_by(City.id).all()
 
-for city in cities:
-    print("{} -> {}".format(city.name, city.state.name))
+    for city in cities:
+        print("{} -> {}".format(city.name, city.state.name))
 
-session.close()
+    session.close()
