0dccd5e275fa56ae69c9960eb4f5d31a3422a3e9
HassanMunene <awanzihassan@gmail.com>
2023-06-30 11:58:09 +0300

    syntax correction

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 #This script will receive a url from the cli and then use curl to display the size of the body of the response
-curl -w "%{size_download}\n" -o /dev/null -s $1
+curl -s "$1" | wc -c
