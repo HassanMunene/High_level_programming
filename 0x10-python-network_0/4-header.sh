3fbe7104fc694483752acb6e7bb3d3b4f9e6c846
HassanMunene <awanzihassan@gmail.com>
2023-06-30 15:48:25 +0300

    syntax correction

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script take in a URL as an arg and sends a GET request to the URL and displays the body of the response, a header var is also specified
-curl -sX GET -H "X-School-User-ID: 98" "$1"
+curl -sH "X-School-User-ID: 98" "$1"
