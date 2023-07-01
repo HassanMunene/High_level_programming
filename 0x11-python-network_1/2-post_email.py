5a196c00602ce46735b06e725be64e121a0223e6
HassanMunene <awanzihassan@gmail.com>
2023-07-01 11:35:56 +0300

    posting data using POST request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module will take a URL and send a POST request to the URL
+to post an email which is passed as a parameter to the url
+"""
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    value = {"email": sys.argv[2]}
+    data = (urllib.parse.urlencode(value).encode('ascii'))
+    r = urllib.request.Request(url, data)
+    with urllib.request.urlopen(r) as response:
+        body = response.read()
+        print(body.decode(utf-8))
