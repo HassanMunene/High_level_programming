8fa05e821247741ffe2396f0a41ebb3200e482d3
HassanMunene <awanzihassan@gmail.com>
2023-07-01 12:49:00 +0300

    making post request

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This script takes in a letter and sends a post request to
+http://0.0.0.0:5000/search_user with the letter as a param
+the letter must be set in var q
+if no argument is prvided set q=''
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        letter = ""
+    else:
+        letter = sys.argv[1]
+    payload = {'q': letter}
+    response = request.post("http://0.0.0.0:5000/search_user", data=payload)
+
+    try:
+        json_resp = response.json()
+        if json_resp == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_resp.get('id'), json_resp.get('name')))
+    except ValueError:
+        print("Not a valid JSON")
