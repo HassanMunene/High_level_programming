cfb9eb9f91988fcc8cd4e7c62e9353e52184daaf
HassanMunene <awanzihassan@gmail.com>
2023-07-01 11:46:39 +0300

    handle errors too that come due to requests

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This sript takes in a URL,sends a request to the URL
+displays  the body of the response
+It also has to manage urllib.error.HTTPError exceptions and print the error code
+"""
+import sys
+import urllib.error
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(r) as response:
+            resp = response.read()
+            print(resp.decode('ascii'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
